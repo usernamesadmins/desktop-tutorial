1cb88d24c2a0d644f38955a661c2696f3bc353c4
admin <129410356+usernamesadmins@users.noreply.github.com>
2025-07-13 14:04:31 +0800

    更新 L.js

--- L.js
+++ L.js
@@ -1,7 +1,7 @@
 const py996 = {};
 const py997 = JSON.parse(typeof $response != "undefined" && $response.body || null);
 
-const name = "Lifetime";
+const name = "Yearly";
 const appid = "yearly_free_ref_10usd_off";
 
   
