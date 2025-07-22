40f713aab86ec0915e561dfe9aaba93a26a9fc7a
admin <129410356+usernamesadmins@users.noreply.github.com>
2025-07-22 16:27:31 +0800

    更新 M.js

--- M.js
+++ M.js
@@ -1,28 +1,44 @@
-const py996 = {};
-const py997 = JSON.parse(typeof $response != "undefined" && $response.body || null);
-
-const name = "Monthly";
-const appid = "monthly_19_99";
-
-  
+const Q = {};
+const Q1 = JSON.parse(typeof $response != "undefined" && $response.body || null);
 if (typeof $response == "undefined") {
-  delete $rePquest.hepadAMpieprsP["x-repvenuecat-etag"];
+  delete $request.headers["x-revenuecat-etag"];
   delete $request.headers["X-RevenueCat-ETag"];
+  Q.headers = $request.headers;
+} else if (Q1 && Q1.subscriber) {
+  Q1.subscriber.subscriptions = Q1.subscriber.subscriptions || {};
+  Q1.subscriber.entitlements = Q1.subscriber.entitlements || {};
+  var headers = {};
+  for (var key in $request.headers) {
+  const reg = /^[a-z]+$/;
+  if (key === "User-Agent" && !reg.test(key)) {
+    var lowerkey = key.toLowerCase();
+    $request.headers[lowerkey] = $request.headers[key];
+    delete $request.headers[key];
+    }
+  }
+  var UA = $request.headers['user-agent'];
+  const app = '1';
+  const UAMappings = {
+      'iScape':{ name: 'Monthly', id: 'monthly_19_99'},//8.15
+    };
 
-  py996.headers = $request.headers;
-} else if (py997 && py997.subscriber) {
-  data = {
-    "expires_date": "2025-08-21T09:09:09Z",
-    "original_purchase_date": "2024-01-23T02:33:33Z",
-    "purchase_date": "2024-01-23T02:33:33Z",
+  const data = {
+    "expires_date": "2025-08-21T12:00:00Z",
+    "original_purchase_date": "2023-09-01T11:00:00Z",
+    "purchase_date": "2023-09-01T11:00:00Z",
     "ownership_type": "PURCHASED",
     "store": "app_store"
   };
-
-  py997.subscriber.subscriptions[(appid)] = data
-  py997.subscriber.entitlements[(name)] = JSON.parse(JSON.stringify(data));
-  py997.subscriber.entitlements[(name)].product_identifier = (appid);
-  py996.body = JSON.stringify(py997);
+  for (const i in UAMappings) {
+    if (new RegExp(`^${i}`, 'i').test(UA)) {
+      const { name, id } = UAMappings[i];
+      Q1.subscriber.subscriptions = {};
+      Q1.subscriber.subscriptions[id] = data;
+      Q1.subscriber.entitlements[name] = JSON.parse(JSON.stringify(data));
+      Q1.subscriber.entitlements[name].product_identifier = id;
+      break;
+    }
+  }
+  Q.body = JSON.stringify(Q1);
 }
-
-$done(py996);
+$done(Q);
